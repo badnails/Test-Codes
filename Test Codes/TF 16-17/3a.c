b41cc822935819a7eecd0b8409c1ff758b01e9c9
badnails <2205026@cse.buet.ac.bd>
2024-03-23 00:36:51 +0600

    Revert "routine"
    This reverts commit f3a73bbc532c1970e79424d145f7559de52763ec.

--- Test Codes/TF 16-17/3a.c
+++ Test Codes/TF 16-17/3a.c
@@ -44,45 +44,21 @@ void unioni(int a[], int b[], int c[], int m, int n)
 
 void intersect(int a[], int b[], int c[], int m, int n)
 {
-    int cin = 0;
-    for(int i = 0; i < m; i++)
-    {
-        int flag = 0;
-        for(int j = 0; j<n; j++)
-        {
-            if(a[i]==b[j])
-            {
-                for(int k = 0; k<cin; k++)
-                {
-                    if(a[i] == c[k])
-                    {
-                        flag = 1;
-                        break;
-                    }
-                }
-                if(flag == 0)
-                {
-                    c[cin] = a[i];
-                    cin++;
-                    break;
-                }
-            }
-        }
-    } 
+    
 }
 
 
 int main()
 {
     int a[10]={1,1,2,3,4,5,6,6,0,1};
-    int b[10]={12,13,14,1,18,6,20,21};
+    int b[10]={12,13,14,1,18,19,20,21};
     int c[20]={-1};
     for(int i = 0; i < 20; i++)
     {
         printf("%d ", c[i]);
     }
     printf("\n");
-    intersect(a,b,c, 10, 8);
+    unioni(a,b,c, 10, 8);
     for(int i = 0; i < 20; i++)
     {
         printf("%d ", c[i]);
